e05e92d4b914e7e107fb6b163baa9b5028a138a9
Nishant Bansal <nishant.bansal.282003@gmail.com>
2026-09-21 23:58:23 +0530

    smite-ir: add HtlcId PaymentHash and PaymentSecret variable types
    Signed-off-by: Nishant Bansal <nishant.bansal.282003@gmail.com>

--- smite-scenarios/src/executor.rs
+++ smite-scenarios/src/executor.rs
@@ -355,6 +355,9 @@ impl<C: Connection, B: BitcoinRpc, R: TargetRpc> Executor<C, B, R> {
                 Operation::LoadFeatures(b) => Some(Variable::Features(b.clone())),
                 Operation::LoadPrivateKey(k) => Some(Variable::PrivateKey(*k)),
                 Operation::LoadChannelId(id) => Some(Variable::ChannelId(ChannelId::new(*id))),
+                Operation::LoadHtlcId(v) => Some(Variable::HtlcId(*v)),
+                Operation::LoadPaymentHash(h) => Some(Variable::PaymentHash(*h)),
+                Operation::LoadPaymentSecret(s) => Some(Variable::PaymentSecret(*s)),
                 Operation::LoadShutdownScript(variant) => Some(Variable::Bytes(variant.encode())),
                 Operation::LoadChannelType(variant) => Some(Variable::Features(variant.encode())),
                 Operation::LoadTargetPubkeyFromContext => {
